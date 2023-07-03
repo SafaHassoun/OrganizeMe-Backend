de32400edb2043bddb14c8bdb81c950cf4f86236
SafaHassoun <133501590+SafaHassoun@users.noreply.github.com>
2023-07-03 16:43:25 +0300

    Add files via upload

---
+++ controllers/ToDoController.js
@@ -0,0 +1,29 @@
+const ToDoModel = require("../models/ToDoModel");
+
+module.exports.getToDo = async (req, res) => {
+  const toDo = await ToDoModel.find();
+  res.send(toDo);
+};
+
+module.exports.saveToDo = async (req, res) => {
+  const { text } = req.body;
+  ToDoModel.create({ text }).then((data) => {
+    console.log("Data added successfuly...");
+    console.log(data);
+    res.send(data);
+  });
+};
+
+module.exports.updateToDo = async (req, res) => {
+  const { _id, text } = req.body;
+  ToDoModel.findByIdAndUpdate(_id, { text })
+    .then(() => res.send("Updated successfuly..."))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDo = async (req, res) => {
+  const { _id } = req.body;
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => res.send("Deleted successfuly..."))
+    .catch((err) => console.log(err));
+};
